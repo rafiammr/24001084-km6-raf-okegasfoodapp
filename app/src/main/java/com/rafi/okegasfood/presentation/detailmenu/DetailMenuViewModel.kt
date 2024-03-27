c5baa357a7b9272b0b1c3564f30c1ca58e4ef253
rafiammr <rafiammr>
2024-03-27 18:21:39 +0700

    Add Feature Detail Menu

--- app/src/main/java/com/rafi/okegasfood/presentation/detailmenu/DetailMenuViewModel.kt
+++ app/src/main/java/com/rafi/okegasfood/presentation/detailmenu/DetailMenuViewModel.kt
@@ -1,6 +1,31 @@
 package com.rafi.okegasfood.presentation.detailmenu
 
+import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.rafi.okegasfood.data.model.Menu
 
-class DetailMenuViewModel : ViewModel() {
+class DetailMenuViewModel(
+    private val extras: Bundle?,
+) : ViewModel() {
+    val menu = extras?.getParcelable<Menu>(DetailMenuActivity.EXTRAS_MENU)
+    private val totalMenu = MutableLiveData<Int>()
+    val totalItemMenu: LiveData<Int> get() = totalMenu
+
+    init {
+        totalMenu.value = 1
+    }
+
+    fun addItem() {
+        val count = totalMenu.value ?: 1
+        totalMenu.value = count + 1
+    }
+
+    fun subItem() {
+        val count = totalMenu.value ?: 1
+        if (count > 1) {
+            totalMenu.value = count - 1
+        }
+    }
 }
